f9782a904a38832577f72b54c876e81b4a015fb2
francois sery <francoissery@pc5.home>
2015-02-18 20:28:09 +0400

    add posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150216094528) do
+ActiveRecord::Schema.define(:version => 20150218101402) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "titre"
+    t.text     "texte"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
